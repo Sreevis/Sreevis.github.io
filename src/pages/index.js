94378fc0694d15431841249edf59ac21b01fdd04
Sreevishnu <sreevishnus18@yahoo.com>
2021-03-30 15:47:38 +0530

    Init test

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>401!</h1>
+      <p>Page not found ...</p>
+    </div>
+  );
+}
